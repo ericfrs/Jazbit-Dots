c9821791d1c6829126eb6667a1a9faecb89292db
Eric A. Farias <erikofarias@gmail.com>
2025-05-10 22:14:46 -0300

    feat: wallpapers for gnome

--- home/home.nix
+++ home/home.nix
@@ -12,6 +12,13 @@
   home.stateVersion = "24.11";
   programs.home-manager.enable = true;
 
+  dconf.settings = {
+    "org/gnome/desktop/background" = {
+      picture-uri = "file://${config.home.homeDirectory}/.config/wallpapers/wall01-l.png";
+      picture-uri-dark = "file://${config.home.homeDirectory}/.config/wallpapers/wall01-d.png";
+    };
+  };
+
   programs.git = {
     enable = true;
     userName = "Eric A. Farias";
